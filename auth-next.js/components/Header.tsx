cbdf889c04774a423e42c47f7eab5201221fd2b1
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-05-11 15:07:22 +0530

    update: audit fixes and changes

--- auth-next.js/components/Header.tsx
+++ auth-next.js/components/Header.tsx
@@ -5,15 +5,17 @@ import { FaHome } from "react-icons/fa";
 
 const Header = () => {
   return (
-    <nav className="absolute w-full flex items-center justify-around p-2">
-      <Link href={'/'} className="text-3xl flex gap-2 p-3 bg-black/15 font-semibold text-white drop-shadow-xl rounded-xl">
-        <FaHome size={35}/>
-        Home.
-      </Link>
-      <div>
-        <LoginButton>
-          <Button variant={'secondary'} size={'lg'}>Sign in</Button>
-        </LoginButton>
+    <nav className="absolute w-full px-20 py-2">
+      <div className="flex items-center justify-around px-2 py-1 rounded-xl bg-white/15 shadow-md">
+        <Link href={'/'} className="text-3xl flex gap-2 p-3 font-semibold text-white drop-shadow-xl rounded-xl">
+          <FaHome size={35}/>
+          Home.
+        </Link>
+        <div>
+          <LoginButton>
+            <Button variant={'secondary'} size={'lg'}>Sign in</Button>
+          </LoginButton>
+        </div>
       </div>
     </nav>
   )
