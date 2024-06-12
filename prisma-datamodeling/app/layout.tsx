e9945da6addd1f9bae45a0d7b85eee069a6d1334
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-06-13 00:44:13 +0530

    wagmi: fixed bugs & improvements

--- prisma-datamodeling/app/layout.tsx
+++ prisma-datamodeling/app/layout.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Prisma Data Modeling",
-  description: "author: abhijayrajvansh",
+  description: "author: Abhijay Rajvansh",
 };
 
 export default function RootLayout({
--- prisma-datamodeling/app/page.tsx
+++ prisma-datamodeling/app/page.tsx
@@ -1,8 +1,8 @@
+import Home from "@/components/Home";
+
 const page = () => {
   return (
-    <div className="flex items-center justify-center h-screen">
-      <p className="text-3xl font-bold">Prisma with Next.js and Postgres</p>
-    </div>
+    <Home />
   );
 };
 
