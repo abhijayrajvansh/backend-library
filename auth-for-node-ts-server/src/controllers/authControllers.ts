3639cdb0c08be91dd7efe90385027462baef320e
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-07-03 06:27:58 +0530

    update: register route & salt

--- auth-for-node-ts-server/src/controllers/authControllers.ts
+++ auth-for-node-ts-server/src/controllers/authControllers.ts
@@ -1,23 +1,48 @@
 import { Request, Response } from "express";
-import prisma from '../db/prisma';
+import prisma from "../db/prisma";
+import { hash } from "bcrypt";
 
 export const test = (req: Request, res: Response) => {
   try {
     return res.status(200).json({
-      msg: 'test-route', status: 200
-    })
+      msg: "test-route",
+      status: 200,
+    });
   } catch (error) {
-    console.log(error.message)
+    console.log(error.message);
   }
-}
+};
 
 export const getUsers = async (req: Request, res: Response) => {
   try {
-    const allUsers = await prisma.users.findMany()
+    const allUsers = await prisma.users.findMany();
     return res.status(200).json({
-      users: allUsers
-    })
+      users: allUsers,
+    });
   } catch (error) {
-    console.log(error.message)
+    console.log(error.message);
   }
-}
+};
+
+export const register = async (req: Request, res: Response) => {
+  try {
+    const { email, password } = await req.body;
+    const saltedPassword = await hash(password, 10);
+
+    const newUser = await prisma.users.create({
+      data: {
+        email, password: saltedPassword
+      }
+    })
+
+    console.log(newUser)
+
+    return res.status(201).json({
+      msg: "user created",
+    });
+  } catch (error) {
+    return res.status(400).json({
+      msg: "user already exists.",
+    });
+  }
+};
--- auth-for-node-ts-server/src/routes/authRoutes.ts
+++ auth-for-node-ts-server/src/routes/authRoutes.ts
@@ -1,9 +1,10 @@
-import { test, getUsers } from "../controllers/authControllers";
+import { test, getUsers, register } from "../controllers/authControllers";
 import { Router } from "express";
 
 const router = Router();
 
 router.get("/test", test);
-router.get('/get-users', getUsers)
+router.get("/users", getUsers);
+router.post("/register", register);
 
 export default router;
