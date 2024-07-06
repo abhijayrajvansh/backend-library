c06f5460119e95ea2328aa3f77008efe091dd570
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-07-06 15:21:40 +0530

    todo: implement login validations

--- auth-for-node-ts-server/src/controllers/authControllers.ts
+++ auth-for-node-ts-server/src/controllers/authControllers.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import prisma from "../db/prisma";
-import { hash } from "bcrypt";
+import { genSalt, hash, compare } from "bcrypt";
 
 export const test = (req: Request, res: Response) => {
   try {
@@ -15,7 +15,14 @@ export const test = (req: Request, res: Response) => {
 
 export const getUsers = async (req: Request, res: Response) => {
   try {
-    const allUsers = await prisma.users.findMany();
+    const allUsers = await prisma.users.findMany({
+      select: {
+        id: true,
+        email: true,
+        role: true,
+        updatedAt: true,
+      },
+    });
     return res.status(200).json({
       users: allUsers,
     });
@@ -27,22 +34,82 @@ export const getUsers = async (req: Request, res: Response) => {
 export const register = async (req: Request, res: Response) => {
   try {
     const { email, password } = await req.body;
-    const saltedPassword = await hash(password, 10);
+    const salt = await genSalt(10);
+    const saltedPassword = await hash(password, salt);
 
     const newUser = await prisma.users.create({
       data: {
-        email, password: saltedPassword
-      }
-    })
+        email,
+        password: saltedPassword,
+      },
+    });
 
-    console.log(newUser)
+    console.log(newUser);
 
     return res.status(201).json({
       msg: "user created",
     });
   } catch (error) {
     return res.status(400).json({
-      msg: "user already exists.",
+      error: "user with email already exists.",
+    });
+  }
+};
+
+export const login = async (req: Request, res: Response) => {
+  try {
+    const { email, password } = await req.body;
+
+    // find if user exists or not
+    const isEmail = await prisma.users.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (!isEmail) return res.json({ isEmail: "user not found" });
+
+    // check if the entered password id correct or not
+    const isValidPassword = await compare(password, isEmail.password);
+
+    console.log(isValidPassword);
+
+    return res.status(201).json({
+      msg: "pending",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      msg: "internal server error",
+    });
+  }
+};
+
+export const deleteUser = async (req: Request, res: Response) => {
+  try {
+    const { id } = await req.body;
+
+    // find if user exists or not
+    const isUser = await prisma.users.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!isUser) return res.status(404).json({msg: 'user not found'})
+
+
+    await prisma.users.delete({
+      where: {
+        id
+      }
+    })
+
+    return res.status(201).json({
+      msg: "user deleted successfully",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      msg: "internal server error",
     });
   }
 };
--- auth-for-node-ts-server/src/routes/authRoutes.ts
+++ auth-for-node-ts-server/src/routes/authRoutes.ts
@@ -1,10 +1,20 @@
-import { test, getUsers, register } from "../controllers/authControllers";
+import {
+  test,
+  getUsers,
+  register,
+  login,
+  deleteUser,
+} from "../controllers/authControllers";
 import { Router } from "express";
+import validate from "../middlewares/validate";
+import { authSchema } from "../validators/authValidators";
 
 const router = Router();
 
 router.get("/test", test);
 router.get("/users", getUsers);
-router.post("/register", register);
+router.post("/register", validate(authSchema), register);
+router.post("/login", validate(authSchema), login);
+router.delete("/delete", deleteUser)
 
 export default router;
