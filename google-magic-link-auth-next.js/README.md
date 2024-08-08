ee25aeb00e2abf0e9376260ec455311b308249c2
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-08-08 10:51:15 +0530

    wagmi: bug fixes & improvements

--- google-magic-link-auth-next.js/README.md
+++
@@ -1,5 +0,0 @@
-# Google Oauth with Next.js
-
-Author: [Abhijay Rajvansh](https://abhijayrajvansh.com)
-
-TBW
--- google-magic-link-auth-next.js/app/page.tsx
+++
@@ -1,9 +0,0 @@
-const page = () => {
-  return (
-    <div>
-      <h3>hello world</h3>
-    </div>
-  )
-}
-
-export default page;
