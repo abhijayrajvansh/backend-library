1d299cf1c5edca3e61bb35776d46b44696224b3e
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-08-22 19:39:50 +0530

    wagmi: bug fixes & improvements

--- auth-nextjs/database/schema.ts
+++ auth-nextjs/database/schema.ts
@@ -1,4 +1,4 @@
-import { boolean, timestamp, pgTable, text, primaryKey, integer } from "drizzle-orm/pg-core"
+import { timestamp, pgTable, text, primaryKey, integer } from "drizzle-orm/pg-core"
 import type { AdapterAccountType } from "next-auth/adapters"
 
 export const users = pgTable("user", {
@@ -9,36 +9,36 @@ export const users = pgTable("user", {
   image: text("image"),
 })
  
-// export const accounts = pgTable("account", {
-//     userId: text("userId").notNull().references(() => users.id, { onDelete: "cascade" }),
-//     type: text("type").$type<AdapterAccountType>().notNull(),
-//     provider: text("provider").notNull(),
-//     providerAccountId: text("providerAccountId").notNull(),
-//     refresh_token: text("refresh_token"),
-//     access_token: text("access_token"),
-//     expires_at: integer("expires_at"),
-//     token_type: text("token_type"),
-//     scope: text("scope"),
-//     id_token: text("id_token"),
-//     session_state: text("session_state"),
-//   },
-//   (account) => ({
-//     compoundKey: primaryKey({
-//       columns: [account.provider, account.providerAccountId],
-//     }),
-//   })
-// )
+export const accounts = pgTable("account", {
+    userId: text("userId").notNull().references(() => users.id, { onDelete: "cascade" }),
+    type: text("type").$type<AdapterAccountType>().notNull(),
+    provider: text("provider").notNull(),
+    providerAccountId: text("providerAccountId").notNull(),
+    refresh_token: text("refresh_token"),
+    access_token: text("access_token"),
+    expires_at: integer("expires_at"),
+    token_type: text("token_type"),
+    scope: text("scope"),
+    id_token: text("id_token"),
+    session_state: text("session_state"),
+  },
+  (account) => ({
+    compoundKey: primaryKey({
+      columns: [account.provider, account.providerAccountId],
+    }),
+  })
+)
  
-// export const verificationTokens = pgTable(
-//   "verificationToken",
-//   {
-//     identifier: text("identifier").notNull(),
-//     token: text("token").notNull(),
-//     expires: timestamp("expires", { mode: "date" }).notNull(),
-//   },
-//   (verificationToken) => ({
-//     compositePk: primaryKey({
-//       columns: [verificationToken.identifier, verificationToken.token],
-//     }),
-//   })
-// )
+export const verificationTokens = pgTable(
+  "verificationToken",
+  {
+    identifier: text("identifier").notNull(),
+    token: text("token").notNull(),
+    expires: timestamp("expires", { mode: "date" }).notNull(),
+  },
+  (verificationToken) => ({
+    compositePk: primaryKey({
+      columns: [verificationToken.identifier, verificationToken.token],
+    }),
+  })
+)
