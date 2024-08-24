e78f61aaccfd99b829c0225530311b0e48d2f2d9
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-08-24 16:46:06 +0530

    update: seeding db

--- auth-nextjs/database/seed.ts
+++ auth-nextjs/database/seed.ts
@@ -1,3 +1,19 @@
-console.log('> seeding database...');
-console.log('> ...')
-console.log('> databse seeded successfully!');
+import { db } from "@/database";
+import { users } from "@/database/schema";
+import { exit } from "process";
+
+async function seed() {
+  try {
+    await db.insert(users).values({
+      name: "bobby",
+      email: "bob@email.com",
+      role: 'ADMIN'
+    });
+  } catch (error) {
+    console.error("caught error while seeding db:", error);
+  }
+}
+
+seed()
+  .then(() => {console.log('\n> db seeded successfully! 🌱')})
+  .finally(exit)
