e6a26a605bbe5aa195ef0241b7615715ee69a808
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-10-02 15:54:54 +0530

    update: dotenv for drizzle db and scripts

--- config/drizzle-index.ts
+++ config/drizzle-index.ts
@@ -1,5 +1,6 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
+import 'dotenv/config'
 
 const connectionString = process.env.DATABASE_URL as string;
 const pool = postgres(connectionString, { max: 1 })
