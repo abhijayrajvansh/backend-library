e42721b86cb003450b4983d8de417290ad930916
Abhijay Rajvansh <abhijayrajvansh01@gmail.com>
2024-10-02 13:37:59 +0530

    update: drizzle.config.ts

--- config/drizzle.config.ts
+++ config/drizzle.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
   dialect: "postgresql",
-  schema: "./database/schema.ts",
-  out: "./database/migrations",
+  schema: "./db/schema.ts",
+  out: "./db/migrations",
   dbCredentials: { url: process.env.DATABASE_URL as string },
   
   // security major
